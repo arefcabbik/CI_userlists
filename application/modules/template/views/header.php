fd9e4b891028d1ca045f24d31091d1bf2515d43e
azism <kakek.anis@gmail.com>
2016-03-14 03:45:00 +0700

    edit header.php in application/modules/template/view/

--- application/modules/template/views/header.php
+++ application/modules/template/views/header.php
@@ -17,16 +17,32 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-form-validator/2.2.43/jquery.form-validator.min.js"></script>
 <!-- SweetAlert JS -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
+
+<script type="text/javascript">
+$(window).load(function() {
+	$(".loader").fadeOut("slow");
+})
+</script>
 </head>
 
 <style>
 #content {
 	margin-top: 10px;
 }
+
+.loader {
+	position: fixed;
+	left: 0px;
+	top: 0px;
+	width: 100%;
+	height: 100%;
+	z-index: 9999;
+	background: url('<?php echo base_url('assets/images/loading.gif');?>') 50% 50% no-repeat;
+}
 </style>
 
 <body>
-
+<div class="loader"></div>
 	<nav>
 	<div class="nav-wrapper">
 		<a href="#" class="brand-logo center">Daftar Pengguna</a>
